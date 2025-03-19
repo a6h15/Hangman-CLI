51062b8cce6b4772aec296c937c075031fb65a1c
a6h1 <abhi.r1509.git@gmail.com>
2025-03-19 18:29:59 +0530

    creating a framework for the cli game

---
+++ hangman.py
@@ -0,0 +1,9 @@
+import random
+
+word_list = ["engineer", "alternatively", "camel"]
+
+def isIntheWord():
+    for i in range(0, len(chosen_word)):
+        if chosen_word[i] == guess:
+            return 1
+    return 0
