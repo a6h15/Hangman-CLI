f90aee2c397f941a43ae280072c8078e749a45fa
a6h1 <abhi.r1509.git@gmail.com>
2025-03-20 20:40:32 +0530

    adding hangman ascii

--- hangman.py
+++ hangman.py
@@ -1,5 +1,62 @@
 import random
 
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', r'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 def guessCorrect(placeholder, chosen_word, guess):
     placeholderList = list(placeholder)
 
@@ -9,10 +66,9 @@ def guessCorrect(placeholder, chosen_word, guess):
 
     return ''.join(placeholderList)
 
-word_list = ["aardvark", "baboon", "camel"]
-
+word_list = ["bankai", "pokemon", "messi"]
 chosen_word = random.choice(word_list)
-print(chosen_word)
+print(f"{chosen_word}")
 
 placeholder = ""
 word_length = len(chosen_word)
@@ -20,7 +76,23 @@ for position in range(word_length):
     placeholder += "_"
 print(placeholder)
 
-while "_" in placeholder:
+lives = 6
+
+
+while "_" in placeholder and lives > 0:
     guess = input("Guess a letter: ").lower()
-    placeholder = guessCorrect(placeholder, chosen_word, guess)
-    print(f"{placeholder}")
+
+    if guess in chosen_word:
+        placeholder = guessCorrect(placeholder, chosen_word, guess)
+        print(stages[lives])
+    else:
+        lives -= 1
+        print(stages[lives])
+
+
+    print(f"Current word: {placeholder}")
+
+if "_" not in placeholder:
+    print(f"Congratulations! You've guessed the word:{chosen_word}")
+else:
+    print(f"You lost!")
